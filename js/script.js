b0154481867a48f929afea5702167bedcda67d53
IIODERII <oderrisi@gmail.com>
2023-10-10 16:15:30 +0200

    added variables and randomnumbers to script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ const generate = document.getElementById("generate");
 
 generate.addEventListener('click', 
     function() {
-        console.log('ciao');
+        const nome = document.querySelector("#nome").value;
+        const dist = document.querySelector("#dist").value;
+        const age = document.querySelector("#age").value;
+
+        const carrozza = getRndInteger(1 , 10);
+        const code = getRndInteger(10000 , 99999);
+        console.log(carrozza)
+        console.log(code)
     }
 )
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
